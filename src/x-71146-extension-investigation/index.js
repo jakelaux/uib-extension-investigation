16c54126536a48dd6050790298314e8d737d4740
Jacob Laux <jake@esolutionsone.com>
2023-08-01 15:30:49 -0500

    updating

--- src/x-71146-extension-investigation/index.js
+++ src/x-71146-extension-investigation/index.js
@@ -29,10 +29,99 @@ const view = (state, { updateState, dispatch }) => {
 				"OPTION2": 	<h3 className='options-example'>Render OPTION2</h3>
 			}[method]
 		}
-		<form>
-			<input type="text" placeholder="test"></input>
-			<input type="date"></input>
-		</form>
+		{/* 
+			https://picocss.com/ >> PicoCSS documentation 
+			Pico Examples below
+		*/}
+		<nav>
+			<ul>
+				<li><strong>Brand</strong></li>
+			</ul>
+			<ul>
+				<li><a href="#">Link</a></li>
+				<li><a href="#">Link</a></li>
+				<li><a href="#" role="button">Button</a></li>
+			</ul>
+		</nav>
+		<nav aria-label="breadcrumb">
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Category</a></li>
+				<li>Page</li>
+			</ul>
+		</nav>
+		<article>
+			<main class="container">
+				<form>
+					<div class="grid">
+						<label for="firstname">First name
+							<input type="text" id="firstname" name="firstname" placeholder="First name" required />
+						</label>
+						<label for="lastname">Last name
+							<input type="text" id="lastname" name="lastname" placeholder="Last name" required />
+						</label>
+					</div>
+					<label for="email">Email address</label>
+					<input type="email" id="email" name="email" placeholder="Email address" required />
+					<small>We'll never share your email with anyone else.</small>
+					<button type="submit">Submit</button>
+				</form>
+
+				<input type="text" placeholder="Valid" aria-invalid="false" />
+				<input type="text" placeholder="Invalid" aria-invalid="true" />
+				<input type="text" placeholder="Disabled" disabled />
+				<input type="text" value="Readonly" readonly></input>
+
+				<table>
+					<thead>
+						<tr>
+						<th scope="col">#</th>
+						<th scope="col">Heading</th>
+						<th scope="col">Heading</th>
+						<th scope="col">Heading</th>
+						<th scope="col">Heading</th>
+						<th scope="col">Heading</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+						<th scope="row">1</th>
+						<td>Cell</td>
+						<td>Cell</td>
+						<td>Cell</td>
+						<td>Cell</td>
+						<td>Cell</td>
+						</tr>
+						<tr>
+						<th scope="row">1</th>
+						<td>Cell</td>
+						<td>Cell</td>
+						<td>Cell</td>
+						<td>Cell</td>
+						<td>Cell</td>
+						</tr>
+						<tr>
+						<th scope="row">1</th>
+						<td>Cell</td>
+						<td>Cell</td>
+						<td>Cell</td>
+						<td>Cell</td>
+						<td>Cell</td>
+						</tr>
+					</tbody>
+					<tfoot>
+						<tr>
+						<th scope="col">#</th>
+						<td scope="col">Total</td>
+						<td scope="col">Total</td>
+						<td scope="col">Total</td>
+						<td scope="col">Total</td>
+						<td scope="col">Total</td>
+						</tr>
+					</tfoot>
+				</table>
+				</main>
+			</article>
 		</div>
 	);
 };
